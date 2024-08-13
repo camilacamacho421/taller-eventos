09f53d0f2f9aedb77ce707390e0eb3b89f712e21
camilacamacho421 <camilacamacho421@gmail.com>
2024-08-12 21:58:22 -0300

    Cambio stopPropagation()

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const div = document.querySelector("div");
+const button = div.querySelector("button");
 
 div.addEventListener("click", () => {
     alert("Hola! Soy el div");
 })
+
+button.addEventListener("click", (event) => {
+    event.stopPropagation(); // Evita que el evento se propague al div
+    alert("Hola!");
+});
